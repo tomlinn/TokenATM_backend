2c3bd074661b9b1b8a5b61ee7c103a2c6511c5f9
Ching-Yang Lin <chingyang886@gmail.com>
2023-02-16 20:00:43 -0800

    add request API

---
+++ token-service/src/main/java/com/capstone/tokenatm/service/RequestRepository.java
@@ -0,0 +1,22 @@
+package com.capstone.tokenatm.service;
+
+import java.util.List;
+
+import com.capstone.tokenatm.entity.SpendLogEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called LogRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface LogRepository extends CrudRepository<SpendLogEntity, Integer> {
+
+    @Query("SELECT l from SpendLogEntity l WHERE l.user_id = ?1")
+    Iterable<SpendLogEntity> findByUserId(String user_id);
+
+    @Query("SELECT l from SpendLogEntity l WHERE l.user_id = ?1 and l.source = ?2 and l.type = ?3")
+    List<SpendLogEntity> findByUserIdAssignmentId(String user_id, String assignment_id, String type);
+
+    @Query("SELECT l from SpendLogEntity l WHERE l.user_name = ?1")
+    Iterable<SpendLogEntity> findByUserName(String user_name);
+}
